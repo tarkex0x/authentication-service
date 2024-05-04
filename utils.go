c841e6dcbaf580dc98c8439d2fb9f348b095d451
tarkex0x <163349630+tarkex0x@users.noreply.github.com>
2024-05-04 12:47:51 +0300

    improving variables

--- utils.go
+++ utils.go
@@ -1,45 +1,45 @@
 package utils
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-	"os"
+    "encoding/json"
+    "log"
+    "net/http"
+    "os"
 
-	"github.com/joho/godotenv"
+    "github.com/joho/godotenv"
 )
 
-func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+func InitializeEnvironmentVariables() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 }
 
-func GetEnv(key, defaultValue string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		return defaultValue
-	}
-	return value
+func EnvironmentVariableOrDefault(key, fallbackValue string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        return fallbackValue
+    }
+    return value
 }
 
-func RespondWithError(w http.ResponseWriter, statusCode int, msg string) {
-	RespondWithJSON(w, statusCode, map[string]string{"error": msg})
+func RespondWithError(writer http.ResponseWriter, statusCode int, errorMessage string) {
+    RespondWithJSON(writer, statusCode, map[string]string{"error": errorMessage})
 }
 
-func RespondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("HTTP 500: Internal Server Error"))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	w.Write(response)
+func RespondWithJSON(writer http.ResponseWriter, statusCode int, payload interface{}) {
+    jsonResponse, err := json.Marshal(payload)
+    if err != nil {
+        writer.WriteHeader(http.StatusInternalServerError)
+        writer.Write([]byte("HTTP 500: Internal Server Error"))
+        return
+    }
+    writer.Header().Set("Content-Type", "application/json")
+    writer.WriteHeader(statusCode)
+    writer.Write(jsonResponse)
 }
 
-func ValidateRequestData(data interface{}) (bool, string) {
-	return true, ""
+func ValidateRequestPayload(payload interface{}) (isValid bool, errorMessage string) {
+    return true, ""
 }
